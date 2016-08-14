f76f05660882436d70f4faa1a467580abf79b3ef
linpeng109 <linpeng109@aliyun.com>
2016-08-14 21:43:41 +0800

    20160814_2

--- routes/camcard.js
+++ routes/camcard.js
@@ -6,10 +6,43 @@
  */
 var needle = require('needle');
 var fs = require('fs');
+var debug = require('debug')('aaa')
+var asycn = require('async');
+var path = require('path');
+var os = require('os');
+var uuid = require('node-uuid');
+
+const getUUID = function () {
+    const result = uuid.v4();
+    return result;
+}
+
+const getDownload = function () {
+    const result = path.join(os.tmpdir(), "/camcard_image_" + "_" + getUUID() + ".png");
+    return result;
+}
+
+const url = 'http://oap63jhn1.bkt.clouddn.com/20160722_110056.jpg';
+
+var wget = function (callback) {
+    const options = {
+        output: getDownload()
+    }
+    needle.get(url, options, function (err, url, response, body) {
+        if (err) {
+            callback(err, null);
+            return
+        }
+        console.log(options.output)
+        callback(null, options.output);
+
+    })
+}
+
 
 var data = {
     upfile: {
-        file: '/home/jupiter/workset/workspace/node_ocr/20160722_110056.jpg',
+        file: getDownload(),
         type: 'file',
         content_type: 'image/png'
     }
@@ -25,15 +58,27 @@ const options = {
  pass: 'HFD6RGWPSRN59MD6',
  lang: '524287',
  */
-
-needle.post('http://bcr2.intsig.net/BCRService/BCR_VCF2?user=lin.peng@all-pay.com.cn&pass=HFD6RGWPSRN59MD6&lang=524287', data, options, function (err, resp, body) {
-    if (err) {
-        console.error.bind(console, err);
-    }
-})
-    .on('readable', function () {
+const getCamcardImageText = function (callback) {
+    needle.post('http://bcr2.intsig.net/BCRService/BCR_VCF2?user=lin.peng@all-pay.com.cn&pass=HFD6RGWPSRN59MD6&lang=524287', data, options, function (err, resp, body) {
+        if (err) {
+            console.error.bind(console, err);
+            callback(err, null);
+        }
+    }).on('readable', function () {
         var node;
         while (node = this.read()) {
             console.log('got data: ', node.toString());
+            callback(null, node.toString())
         }
     });
+
+}
+
+asycn.waterfall([wget, getCamcardImageText], function (err, result) {
+    if (err) {
+        console.error.bind(console, err);
+
+    }
+    console.log(result);
+
+})
--- routes/ocr.js
+++ routes/ocr.js
@@ -53,27 +53,8 @@ var ocr = function (file, callback) {
         }
     });
 }
-//const imageFile = 'ocr_image__8629f8c3-e3e2-4ef6-9042-21cd11d94ba4.png';
-//ocr_image__6ffb72f7-9144-4d94-b843-efcce9828078.png
-//ocr_image__8629f8c3-e3e2-4ef6-9042-21cd11d94ba4.png
-//ocr_image__a314e818-daab-452e-9a17-c938abdfd820.png
-//ocr_image__a9afcf04-3467-4eaf-b7be-10d80100e631.png
-// wget(function (err, file) {
-//     if (err) {
-//         console.error.bind(console, err);
-//         return;
-//     }
-//
-//
-// })
 
-// ocr('/tmp/ocr_image__4423c4b6-8a2a-4c22-89e4-c60a5561ec67.png', function (err, file, text) {
-//     if (err) {
-//         console.error.bind(console, err)
-//     }
-//     console.log(text);
-// })
-router.get('/',function (req, res) {
+router.get('/', function (req, res) {
     async.waterfall([wget, ocr], function (err, file, text) {
         if (err) {
             console.error.bind(console, err);
