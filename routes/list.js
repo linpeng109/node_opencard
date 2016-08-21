0f25c98bec56e4bc6d7e3eedeb9795fa1941c37d
linpeng109 <linpeng109@aliyun.com>
2016-08-21 15:40:47 +0800

    160821

--- routes/list.js
+++ routes/list.js
@@ -1,20 +1,58 @@
 /**
  * Created by ceres on 16-8-14.
+ * 参考资料
+ * http://note.youdao.com/yws/public/redirect/share?id=d6c1f6afcb84c1ac13d1a3b4925e9725&type=false
  */
 var express = require('express');
 var router = express.Router();
 
+/**
+ * 分页查询
+ */
+router.get('/findByPage', function (req, res, next) {
+    var User = global.dao.User
+    var pageSize = req.query.pageSize;
+    var pageNum = req.query.pageNum;
+    User.find({})
+        .skip(pageSize * (pageNum - 1))
+        .limit(pageSize)
+        .exec(function (err, docs) {
+            if (err) {
+                console.error(err);
+                res.redirect('/failure')
+                return;
+            }
+            res.jsonp(docs);
+        })
+});
 
-
-router.get('/', function (req, res) {
-    var User = global.dao.User;
-    var result={}
-    User.findAll(function (err,result) {
-        if(err){
-            console.error(err);
-        }
-        res.jsonp(result)
-    })
+/**
+ * 全部查询
+ */
+router.get('/findAll', function (req, res, next) {
+    var User = global.dao.User
+    User.find({})
+        .skip(0)
+        .limit(20)
+        .exec(function (err, docs) {
+            if (err) {
+                console.error(err);
+            }
+            res.jsonp(docs)
+        })
 })
 
+/**
+ * 删除
+ */
+router.get('/del', function (req, res, next) {
+    var User = global.dao.User
+    User.remove({_id: '578330f39f4e1a689da4e0fc'}, function (err) {
+        if (err) {
+            console.error(err);
+        } else {
+            res.redirect('/success');
+        }
+    })
+});
 module.exports = router;
